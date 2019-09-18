145ec63478cad689b1f239db16972f7a4893f919
djok0127 <djok0127@gmail.com>
2019-09-17 22:01:50 -0700

    coin finished

---
+++ Chris/Assets/coinColletor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinColletor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coins"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
