0609dce3c3ae0142b63459e92aa46f470fa1fce1
butterscreen <55117962+butterscreen@users.noreply.github.com>
2019-12-02 17:57:52 -0800

    Pause Menu Stuff

---
+++ Duy/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public TMP_Dropdown resolutionsDropdown;
+    public Slider Slider;
+
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        audioMixer.SetFloat("Volume", 1.0f);
+        resolutions = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i =0; i < resolutions.Length; i++){
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && 
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = currentResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+    }
+    public void setVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
